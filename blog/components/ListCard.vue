11f12fa74cbd3adf2227c1f2e87a59b275e07ddb
Vladislav <chey824969@gmail.com>
2023-11-25 20:45:41 +0300

    add pagination

--- blog/components/ListCard.vue
+++ blog/components/ListCard.vue
@@ -1,33 +1,59 @@
 <script setup lang="ts">
-  import {useAsyncData} from "#app";
+import {useAsyncData} from "#app";
+import {computed} from "vue";
 
-  const msg = 'Hello!'
-  const url = 'https://6082e3545dbd2c001757abf5.mockapi.io/qtim-test-work/posts/'
+const API = 'https://6082e3545dbd2c001757abf5.mockapi.io/qtim-test-work/posts/'
+const dataList = ref()
+const {data, pending, error, refresh} = await useAsyncData('list', () => $fetch(API))
+dataList.value = data.value
 
-  const { data } = await useAsyncData('data', () => $fetch(url))
+const activePage = ref(1)
+const cardNumber = ref(8)
 
-
-  const arr = ref([])
-  function log() {
-    console.log(msg)
-    console.warn('arr: ', arr)
+const nextPage = function () {
+  if (activePage.value < numberPage.value) {
+     activePage.value++
+  } else {
+    activePage.value = 1
   }
+}
 
+const activeData = computed(() => {
+  return dataList.value.slice(activePage.value * cardNumber.value - 1, activePage.value * cardNumber.value * 2)
+})
+
+const pageCount = ref(0)
+
+const numberPage = computed(() => Math.floor(dataList.value.length / 8))
+pageCount.value = numberPage.value
+const pages = ref([])
 
 </script>
 
 <template>
   <div>
-    <ul>
+    <button class="tw-p-5 tw-m-5 tw-bg-blog-blue-btn" @click="nextPage">` > `</button>
+    <strong class="tw-text-amber-950 tw-p-[10px] tw-m-5 tw-block">{{ activePage }}</strong>
+    <ul class="tw-flex tw-flex-row tw-py-[50px]">
       <li>
-        <button @click="log">{{ msg }}</button>
+        <button class="tw-mx-[30px] tw-p-[10px] tw-bg-blog-red-main tw-text-white">
+          1
+        </button>
       </li>
-      <li
-        v-for="(item, index) in arr"
-        :key="index">
-        <NuxtLink :to="`/card/${item}`">Blog: {{ item }}</NuxtLink>
+      <li>
+        <button class="tw-mx-[30px] tw-p-[10px] tw-bg-blog-red-main tw-text-white">
+          2
+        </button>
+      </li>
+
+    </ul>
+    <ul v-if="Array.isArray(activeData)">
+      <li v-for="(item, index) in activeData" :key="index" class="tw-p-[30px]">
+        <div>{{ item }}</div>
       </li>
     </ul>
+    <div>Go to Page</div>
+
   </div>
 </template>
 
